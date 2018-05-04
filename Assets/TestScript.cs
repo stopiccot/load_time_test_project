b533a42370f08fe6f74d4ead91477137eb1de20a
stopiccot <alexey.petruchik@gmail.com>
2018-05-04 17:14:44 +0300

    initial commit

---
+++ Assets/TestScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ButtonClick() 
+    {
+        var rt = GetComponent<RectTransform>();
+        var ap = rt.anchoredPosition;
+        ap.y += 20;
+        rt.anchoredPosition = ap;
+    }
+}
